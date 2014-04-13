122e42bb7c2114cf56c3e926bfe1a5b97d4479ce
songb2 <brucesong2010@hotmail.com>
2014-04-13 21:26:35 +0800

    Model Validation: custom property validation attribute and model validation attribute

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,16 +1,23 @@
 ﻿
+using ModelValidation.Infrastructure;
 using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace ModelValidation.Models
 {
+    [NoJoeOnMonday]
     public class Appointment
     {
+        [Required]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
+        [FutureDate(ErrorMessage = "Please enter a date in the future")]    
         public DateTime Date { get; set; }
 
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+
+        [MustBeTrue(ErrorMessage="You must accept the terms.")]
         public bool TermsAccepted { get; set; }
     }
 }
